ae771363c3c76adf6d172b4104d9a3754539a7e9
Ryan Jones <ryan.jones26@gmail.com>
2015-01-07 15:22:01 -0500

    finished  basic carrier model
    Still needs validations

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var carriers = require('.models/carriers');
+var carriers = require('./routes/carriers');
 
 var app = express();
 
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/carrers', carriers);
+app.use('/carriers', carriers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/carrier.js
+++ models/carrier.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 var CarrierSchema = new mongoose.Schema({
     carrier_name: String,
     support_email: String,
-    support_num: String
+    support_num: String,
+    carrier_street: String,
+    carrier_city: String,
+    carrier_state: String,
+    carrier_postal: String,
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now},
+    
 });
 module.exports = mongoose.model('Carrier', CarrierSchema);
