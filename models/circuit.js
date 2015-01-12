31bef83650140fda56fca5e14bb928eaed5a4e42
Ryan Jones <ryan.jones26@gmail.com>
2015-01-11 17:08:12 -0800

    added two more fields to the carrier schema

--- models/circuit.js
+++ models/circuit.js
@@ -5,6 +5,8 @@ var CircuitSchema = new mongoose.Schema( {
     
     circuit_id: String,
     circuit_loc: String,
+    carrier_block : String,
+    assigned_range: String,
     carrier: [ { type: Schema.Types.ObjectId, ref : 'CarrierSchema' }
                                         ]
     });
