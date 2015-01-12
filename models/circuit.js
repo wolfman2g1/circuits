cdb7d57d3589a0eaf378e70dbd88c479a5f3c24b
Ryan Jones <ryan.jones26@gmail.com>
2015-01-11 18:36:44 -0800

    rev to ver 0.1.0 added passport and created a basic user model

--- models/circuit.js
+++ models/circuit.js
@@ -3,14 +3,18 @@ var mongoose =require('mongoose');
 
 var CircuitSchema = new mongoose.Schema( {
     
-    circuit_id: String,
-    circuit_loc: String,
-    carrier_block : String,
-    assigned_range: String,
+    circuit_id : { type : String, required : true},
+    circuit_loc: { type : String, required : true},
+    carrier_block :{ type : String, required : true},
+    assigned_range: { type : String, required : true},
     carrier: [ { type: Schema.Types.ObjectId, ref : 'CarrierSchema' }
                                         ]
     });
 
 /* start validation of data */
+
+
+
+
 module.exports = mongoose.model('Circuit', CircuitSchema);
                                         
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(req, res,  next) {
+    User.fin(function(err, users){
+        if(err) return next(err);
+        res.json(users);
+    });
+ 
 });
+
+router.post('/', function(req, res, next){
+    User.create(req.body, function(err, post){
+        if(err) return next(err);
+        res.json(post);
+    })
+    
+})
 
 module.exports = router;
