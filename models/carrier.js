fcc38da2e2ad5307fbf258cb4fd986d699ddced2
Ryan Jones <ryan.jones26@gmail.com>
2015-05-04 01:56:12 -0400

    changed the carrier model to  use sequelize. haven't tested yet

--- models/carrier.js
+++ models/carrier.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose');
-
+//var mongoose = require('mongoose');
+//import the new library
+var Sequelize = require('sequelize');
 /* create the carrier Schema for api access */
-var CarrierSchema = new mongoose.Schema({
+/*var CarrierSchema = new mongoose.Schema({
     carrier_name: String,
     support_email: { type : String, required : true, match: /\S+@\S+\.\S+/ },
     support_num: String,
@@ -13,6 +14,17 @@ var CarrierSchema = new mongoose.Schema({
     updated_at: { type: Date, default: Date.now},
     
 });
+//TODO clean up old mongoose coode
+*/
+// create the new carrier model
 
-
+var Carrier = new sequelize.define("Carrier", {
+    carrier_name: Sequelize.STRING,
+    support_email: Sequelize.STRING,
+    support_nam: Sequelize.STRING,
+    Carrier_street: Sequelize.STRING,
+    carrier_city: Sequelize.STRING,
+    carrier_state: Sequelize.STRING,
+    carrier_postal: Sequelize.STRING
+});
 module.exports = mongoose.model('Carrier', CarrierSchema);
